79207ce671275f1dfe1e20d757695df8391efe81
Tony Tam <tony@eatbacon.org>
2012-01-24 21:33:31 -0800

    removed unused imports

--- modules/swagger-jaxrs/src/main/scala/com/wordnik/swagger/jaxrs/ApiReader.scala
+++ modules/swagger-jaxrs/src/main/scala/com/wordnik/swagger/jaxrs/ApiReader.scala
@@ -19,8 +19,6 @@ package com.wordnik.swagger.jaxrs
 import com.wordnik.swagger.core._
 import com.wordnik.swagger.core.ApiValues._
 import com.wordnik.swagger.core.util.TypeUtil
-import com.wordnik.swagger.jaxrs.ApiReader;
-import com.wordnik.swagger.jaxrs.ApiSpecParser;
 
 import org.slf4j.LoggerFactory
 
