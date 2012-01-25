bf98a8a9458e775585d24d044f2ae789cf35ec76
Tony Tam <tony@eatbacon.org>
2012-01-25 06:16:15 -0800

    added integration tests

---
+++ samples/java-jaxrs/src/test/scala/com/wordnik/test/swagger/integration/ResourceListingIT.scala
@@ -0,0 +1,21 @@
+package com.wordnik.test.swagger.integration
+
+import com.wordnik.swagger.core._
+import com.wordnik.swagger.core.util.JsonUtil
+
+import org.junit.runner.RunWith
+
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+
+import scala.io._
+
+@RunWith(classOf[JUnitRunner])
+class ResourceListingIT extends FlatSpec with ShouldMatchers {
+  it should "read a resource listing" in {
+    val json = Source.fromURL("http://localhost:8002/api/resources.json").mkString
+    val doc = JsonUtil.getJsonMapper.readValue(json, classOf[Documentation])
+    println(doc)
+  }
+}
