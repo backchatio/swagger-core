7f605c5820696069c4d0b6a97b53ff691bdd5c1f
Tony Tam <tony@eatbacon.org>
2012-01-25 09:21:20 -0800

    added xml tests

--- samples/java-jaxrs/src/test/scala/com/wordnik/test/swagger/integration/ResourceListingIT.scala
+++ samples/java-jaxrs/src/test/scala/com/wordnik/test/swagger/integration/ResourceListingIT.scala
@@ -1,6 +1,7 @@
 package com.wordnik.test.swagger.integration
 
 import com.wordnik.swagger.core._
+
 import com.wordnik.swagger.core.util.JsonUtil
 
 import org.junit.runner.RunWith
@@ -22,14 +23,28 @@ class ResourceListingIT extends FlatSpec with ShouldMatchers {
     assert((doc.getApis.map(api => api.getPath).toSet & Set("/pet.{format}", "/user.{format}")).size == 2)
   }
 
+  it should "read the resource listing in XML" in {
+    val xmlString = Source.fromURL("http://localhost:8002/api/resources.xml").mkString
+    val xml = scala.xml.XML.loadString(xmlString)
+    assert(((xml \ "apis").map(api => (api \ "path").text).toSet & Set("/pet.{format}", "/user.{format}")).size == 2)
+  }
+
   it should "read the pet api description" in {
     val json = Source.fromURL("http://localhost:8002/api/pet.json").mkString
     val doc = JsonUtil.getJsonMapper.readValue(json, classOf[Documentation])
-    println(JsonUtil.getJsonMapper.writeValueAsString(doc))
     assert(doc.getApis.size === 3)
-    println(doc.getApis.map(api => api.getPath).toSet)
-    assert((doc.getApis.map(api => api.getPath).toSet & 
-        Set("/pet.{format}/{petId}",
+    assert((doc.getApis.map(api => api.getPath).toSet &
+      Set("/pet.{format}/{petId}",
+        "/pet.{format}/findByStatus",
+        "/pet.{format}/findByTags")).size == 3)
+  }
+
+  it should "read the pet api description in XML" in {
+    val xmlString = Source.fromURL("http://localhost:8002/api/pet.xml").mkString
+    val xml = scala.xml.XML.loadString(xmlString)
+
+    assert(((xml \ "apis").map(api => (api \ "path").text).toSet &
+      Set("/pet.{format}/{petId}",
         "/pet.{format}/findByStatus",
         "/pet.{format}/findByTags")).size == 3)
   }
