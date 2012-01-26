2f7bd64ad38c3875858c0dc88c572151627be9b0
Tony Tam <tony@eatbacon.org>
2012-01-25 16:22:39 -0800

    enabled tests

--- samples/scala-jaxrs/src/test/scala/com/wordnik/test/swagger/integration/ResourceListingIT.scala
+++ samples/scala-jaxrs/src/test/scala/com/wordnik/test/swagger/integration/ResourceListingIT.scala
@@ -19,17 +19,17 @@ class ResourceListingIT extends FlatSpec with ShouldMatchers {
     val json = Source.fromURL("http://localhost:8002/api/resources.json").mkString
     val doc = JsonUtil.getJsonMapper.readValue(json, classOf[Documentation])
     assert(doc.getApis.size === 2)
-    assert((doc.getApis.map(api => api.getPath).toSet & Set("/pet.{format}", "/user.{format}")).size == 2)
+    assert((doc.getApis.map(api => api.getPath).toSet &
+      Set("/pet.{format}",
+        "/user.{format}")).size == 2)
   }
 
   it should "read the pet api description" in {
     val json = Source.fromURL("http://localhost:8002/api/pet.json").mkString
     val doc = JsonUtil.getJsonMapper.readValue(json, classOf[Documentation])
-    println(JsonUtil.getJsonMapper.writeValueAsString(doc))
     assert(doc.getApis.size === 3)
-    println(doc.getApis.map(api => api.getPath).toSet)
-    assert((doc.getApis.map(api => api.getPath).toSet & 
-        Set("/pet.{format}/{petId}",
+    assert((doc.getApis.map(api => api.getPath).toSet &
+      Set("/pet.{format}/{petId}",
         "/pet.{format}/findByStatus",
         "/pet.{format}/findByTags")).size == 3)
   }
